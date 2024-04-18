1d528bb6d2e9db30ee78f5a2c421337ed157b21d
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-04-18 15:18:02 +1200

    init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ImageDialog from './components/ImageDialog.vue'
+</script>
+
+<template>
+  <ImageDialog msg="Image Dialog" />
+</template>
---
+++ src/components/ImageDialog.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const data = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p>{{ data }}</p>
+</template>
+
+<style scoped>
+
+</style>
